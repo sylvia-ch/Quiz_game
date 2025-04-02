f34abc11f12b09a0d22ba66a7bc011b92449220a
Sylvia chen <siyu.chen@my.bdsc.school.nzehdqegit config --global user.name Sylvia>
2025-04-03 09:26:44 +1300

    version1 first change

--- Quiz_game.py
+++ Quiz_game.py
@@ -1,2 +1,8 @@
 print("This is my first commit")
 print("Hello World")
+# Set up project
+import tkinter as tk
+from tkinter import messagebox
+import matplotlib.pyplot as plt
+
+# Code: Create data stucture for quiz and answer
